70c87410ff04dc0caa7663f5f7c02e54cf9d5216
Jee <jee@jeer.fr>
2021-03-21 16:21:17 +0100

    clean unused def

--- tests/logger_test.py
+++ tests/logger_test.py
@@ -44,22 +44,5 @@ class TestLogger(unittest.TestCase):
 
         self.assertEqual(self.logger_level, levelname)
 
-
-#    def test_check_duration_(self):
-#        self.assertEqual(50, self.config.check_duration(50))
-#        with self.assertRaises(Exception): self.config.check_duration("aString")
-#
-#    def test_interval_duration(self):
-#        self.assertEqual({'days': 5}, self.config.interval_validation("{'days': 5}"))
-#        with self.assertRaises(Exception): self.config.interval_validation(50)
-#        with self.assertRaises(Exception): self.config.interval_validation("{'years': 5}")
-#        with self.assertRaises(Exception): self.config.interval_validation({})
-#        with self.assertRaises(Exception): self.config.interval_validation("thisIsAString")
-#        with self.assertRaises(Exception): self.config.interval_validation("/this/path/dont/exist")
-#
-#    def test_check_chunkSize(self):
-#        self.assertEqual(50, self.config.check_chunkSize(50))
-#        with self.assertRaises(Exception): self.config.check_chunkSize("aString")
-
 if __name__ == '__main__':
     unittest.main()
